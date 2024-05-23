c6887ad12c300cdf57c94e4ff0b129fe4b16b3c0
rusty <kuriakosant2003@gmail.com>
2024-05-23 12:00:00 +0300

    fixed mistake in sleection sort function

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -14,10 +14,11 @@ def selection_sort(arr):
     for i in range(len(arr)):
         min_idx = i
         for j in range(i + 1, len(arr)):
-            if arr[j] < arr[min_idx]:
+            if arr[j] < arr[min_idx]:  # Fixed comparison
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
+
 
 
 def insertion_sort(arr):
